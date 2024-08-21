9c3f9a79e303b73fbedf52d1f81a15f4d24ee1f3
Vinicius <porcionatovinicius@gmail.com>
2024-08-21 00:15:16 -0300

    todolist

---
+++ todolist/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import MainContent from './components/MainContent/MainContent';
+import Container from './components/Container/Container';
+
+function App() {
+  return (
+    <div className='todolist'>
+    <MainContent>
+      <Container>
+
+      </Container>
+    </MainContent>
+    </div>
+  );
+}
+
+export default App;
